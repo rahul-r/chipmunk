28ed6fd5ea8ed86927beadd1f58abfb69051500d
rahul <jossb6cw@duck.com>
2024-05-14 22:53:39 -0700

    Update ci.yml

--- chipmunk/src/database/mod.rs
+++ chipmunk/src/database/mod.rs
@@ -10,6 +10,7 @@ pub mod teslamate;
 pub use teslamate::Teslamate;
 
 pub async fn initialize(url: &str) -> anyhow::Result<PgPool> {
+    log::info!("Commecting to postgres database `{url}`");
     let pool = PgPool::connect(url).await?;
 
     log::info!("Running database migrations");
