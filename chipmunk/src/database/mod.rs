242c3cdbb517018eb02090830fd65eebaf4a8a02
rahul <jossb6cw@duck.com>
2024-05-15 06:19:46 -0700

    Temporerly disable failing test to test GitHub action

--- chipmunk/src/database/mod.rs
+++ chipmunk/src/database/mod.rs
@@ -10,7 +10,7 @@ pub mod teslamate;
 pub use teslamate::Teslamate;
 
 pub async fn initialize(url: &str) -> anyhow::Result<PgPool> {
-    log::info!("Commecting to postgres database `{url}`");
+    log::info!("Connecting to postgres database");
     let pool = PgPool::connect(url).await?;
 
     log::info!("Running database migrations");
